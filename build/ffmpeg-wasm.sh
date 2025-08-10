f160b69d9cd2b9518faf65ee2e13e3db6978b944
Alfred Huang <whatever0426.am03@gmail.com>
2025-08-11 01:05:34 +0800

    在 ffmpeg 構建腳本中新增函數指標優化選項，以進一步提升生成的二進制檔案性能和構建效率。

--- build/ffmpeg-wasm.sh
+++ build/ffmpeg-wasm.sh
@@ -43,6 +43,7 @@ CONF_FLAGS=(
   -sELIMINATE_DUPLICATE_FUNCTIONS=1        # remove duplicate functions
   -sINLINING_LIMIT=1                       # disable inlining to reduce code size
   -sPRECISE_F32=0                          # use fast math
+  -sALIASING_FUNCTION_POINTERS=1           # optimize function pointer calls
   ${FFMPEG_MT:+ -sINITIAL_MEMORY=1024MB}   # ALLOW_MEMORY_GROWTH is not recommended when using threads, thus we use a large initial memory
   ${FFMPEG_MT:+ -sPTHREAD_POOL_SIZE=32}    # use 32 threads
   ${FFMPEG_ST:+ -sINITIAL_MEMORY=32MB -sALLOW_MEMORY_GROWTH} # Use just enough memory as memory usage can grow
