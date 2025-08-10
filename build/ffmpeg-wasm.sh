5f04dd7e27243f64bfb077c809d642408900f5c7
Alfred Huang <whatever0426.am03@gmail.com>
2025-08-11 00:40:45 +0800

    在 ffmpeg 構建腳本中新增內聯限制選項，以進一步減少生成的二進制檔案大小，優化構建效率。

--- build/ffmpeg-wasm.sh
+++ build/ffmpeg-wasm.sh
@@ -41,6 +41,7 @@ CONF_FLAGS=(
   -sDISABLE_EXCEPTION_CATCHING=1           # strip exception handling code
   -sAGGRESSIVE_VARIABLE_ELIMINATION=1      # more aggressive dead code elimination
   -sELIMINATE_DUPLICATE_FUNCTIONS=1        # remove duplicate functions
+  -sINLINING_LIMIT=1                       # disable inlining to reduce code size
   ${FFMPEG_MT:+ -sINITIAL_MEMORY=1024MB}   # ALLOW_MEMORY_GROWTH is not recommended when using threads, thus we use a large initial memory
   ${FFMPEG_MT:+ -sPTHREAD_POOL_SIZE=32}    # use 32 threads
   ${FFMPEG_ST:+ -sINITIAL_MEMORY=32MB -sALLOW_MEMORY_GROWTH} # Use just enough memory as memory usage can grow
