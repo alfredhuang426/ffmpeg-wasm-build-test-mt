65341fc7e9ad89f7075d612251567b2797d58ba4
alfred huang <alfred.huang@aiello.ai>
2025-08-11 13:59:40 +0800

    調整 ffmpeg 構建腳本，激進禁用多個大型編碼器和解碼器選項，以進一步精簡生成的二進制檔案大小和提高構建效率。

--- build/ffmpeg.sh
+++ build/ffmpeg.sh
@@ -34,18 +34,35 @@ CONF_FLAGS=(
   --disable-outdevs
   --disable-devices
   
-  # 編碼器 - 禁用大型/不需要的編碼器，保留 libx264
-  --disable-encoder=libx265,libxvid,libvpx,libvpx-vp9,libaom-av1
-  --disable-encoder=libtheora,libvorbis,libopus,libmp3lame
-  --disable-encoder=wmv1,wmv2,wmv3,vc1,h263,h261
-  --disable-encoder=flac,ape,tta,shn,ra_144,ra_288,ralf
+  # 編碼器 - 激進禁用，只保留 libx264 和基本編碼器
+  --disable-encoder=libx265,libxvid,libvpx,libvpx-vp9,libaom-av1,libtheora
+  --disable-encoder=libvorbis,libopus,libmp3lame,libfdk_aac,libfaac
+  --disable-encoder=wmv1,wmv2,wmv3,vc1,h263,h261,mpeg1video,mpeg2video
+  --disable-encoder=flac,ape,tta,shn,ra_144,ra_288,ralf,roq_dpcm
   --disable-encoder=webp,jpeg2000,tiff,bmp,gif,pcx,tga,sgi,iff,dxv,prores
+  --disable-encoder=dnxhd,prores,prores_aw,prores_ks,qtrle
+  --disable-encoder=ffv1,ffvhuuff,huffyuv,utvideo,zlib
+  --disable-encoder=ac3,eac3,dca,dts,truehd,mlp
+  --disable-encoder=amr_nb,amr_wb,gsm,gsm_ms,ilbc
+  --disable-encoder=mp2,mp3,mp3adu,mp3adufloat,mp3float
+  --disable-encoder=wmav1,wmav2,wmavoice,wmapro,wmalossless
   
-  # 解碼器 - 只禁用一些明顯不需要的大型解碼器
+  # 解碼器 - 激進禁用，只保留基本解碼器
   --disable-decoder=vp8,vp9,av1,theora,wmv1,wmv2,wmv3,vc1,flv,h263,h261
   --disable-decoder=mp2,mp3float,mp3adu,mp3adufloat,wmav1,wmav2,wmavoice
   --disable-decoder=flac,ape,tta,shn,ra_144,ra_288,ralf,roq_dpcm
   --disable-decoder=webp,jpeg2000,tiff,bmp,gif,pcx,tga,sgi,iff,dxv,prores
+  --disable-decoder=mpeg1video,mpeg2video,mpeg4,msmpeg4v1,msmpeg4v2,msmpeg4v3
+  --disable-decoder=dnxhd,prores,prores_aw,prores_ks,qtrle
+  --disable-decoder=ffv1,ffvhuuff,huffyuv,utvideo,zlib
+  --disable-decoder=ac3,eac3,dca,dts,truehd,mlp
+  --disable-decoder=amr_nb,amr_wb,gsm,gsm_ms,ilbc
+  --disable-decoder=mp3,mp3adu,mp3adufloat,mp3float
+  --disable-decoder=wmav1,wmav2,wmavoice,wmapro,wmalossless
+  --disable-decoder=opus,vorbis,speex
+  --disable-decoder=rv10,rv20,rv30,rv40
+  --disable-decoder=indeo2,indeo3,indeo4,indeo5
+  --disable-decoder=cyuv,tscc,msrle,qpeg,qtrle
   
   # Muxer - 支援常見容器
   --disable-muxers --enable-muxer=mp4,mov,avi,webm
